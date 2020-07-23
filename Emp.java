aea7af9a2b2177607a5627fa8352548d03b6a67a
abhishek <abhishek96890@gmail.com>
2020-07-23 22:39:26 +0530

    ADDED:Month

--- Emp.java
+++ Emp.java
@@ -2,11 +2,13 @@ public class Emp{
    public static final int IS_PART_TIME = 1;
    public static final int IS_FULL_TIME = 2;
    public static final int EMP_RATE_PER_HOUR = 20;
+   public static final int NUM_OF_WORKING_DAYS =2;
 public static void main(String args[])
 {
 
-int empHrs = 0;
-int empwage = 0;
+int empHrs = 0, empwage = 0, totalEmpWage=0;
+for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
+{ 
 int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 switch (empcheck) 
 {
@@ -20,7 +22,9 @@ switch (empcheck)
         empHrs = 0;
 }
 EmpWage = empHrs * EMP_RATE_PER_HOUR;
+totalEmpWage += empWage;
 System.out.println("Emp Wage: " + EmpWage);
-
+}
+System.out.println("Total Emp Wage: " + totalEmpWage);
 }
 }
