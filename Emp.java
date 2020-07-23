46f41f40731206dc5ac3c134497fa6255f381034
abhishek <abhishek96890@gmail.com>
2020-07-23 22:09:57 +0530

    ADDED:AddPart

--- Emp.java
+++ Emp.java
@@ -2,13 +2,16 @@ public class Emp{
 
 public static void main(String args[])
 {
-int IS_FULL_TIME = 1;
+int IS_PART_TIME = 1;
+int IS_FULL_TIME = 2;
 int EMP_RATE_PER_HOUR = 20;
 int empHrs = 0;
 int empwage = 0;
-double empcheck = Math.floor(Math.random() * 10) % 2;
-if (Empcheck == IS_FULL_TIME)
-     empHrs =  8;
+double empcheck = Math.floor(Math.random() * 10) % 3;
+if (empcheck == IS_FULL_TIME)
+     empHrs = 4;
+else if (empcheck == IS_FULL_TIME)
+     empHrs = 8;
 else
      empHrs = 0;
 EmpWage = empHrs * EMP_RATE_PER_HOUR;
