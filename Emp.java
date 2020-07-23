e456176e6ea43bdf5f0eb0b9a6f50b82a51ea8f0
abhishek <abhishek96890@gmail.com>
2020-07-23 21:14:39 +0530

    ADDED:Check

--- Emp.java
+++ Emp.java
@@ -2,6 +2,11 @@ public class Emp{
 
 public static void main(String args[])
 {
-System.out.println("Welcome");
-}
+int IS_FULL_TIME = 1;
+double empcheck = Math.floor(Math.random() * 10) % 2;
+if (empcheck == IS_FULL_TIME)
+    System.out.println("Employee is Present");
+else
+    System.out.println("Employee is Abscent");
+  }
 }
