826cacbc1bceb1cda15b39059535c95fce9ec18d
abhishek <abhishek96890@gmail.com>
2020-07-23 22:23:08 +0530

    ADDED:Switch

--- Emp.java
+++ Emp.java
@@ -1,19 +1,24 @@
 public class Emp{
-
+   public static final int IS_PART_TIME = 1;
+   public static final int IS_FULL_TIME = 2;
+   public static final int EMP_RATE_PER_HOUR = 20;
 public static void main(String args[])
 {
-int IS_PART_TIME = 1;
-int IS_FULL_TIME = 2;
-int EMP_RATE_PER_HOUR = 20;
+
 int empHrs = 0;
 int empwage = 0;
-double empcheck = Math.floor(Math.random() * 10) % 3;
-if (empcheck == IS_FULL_TIME)
-     empHrs = 4;
-else if (empcheck == IS_FULL_TIME)
-     empHrs = 8;
-else
-     empHrs = 0;
+int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+switch (empcheck) 
+{
+   case IS_PART_TIME:
+        empHrs = 4;
+        break;
+   case IS_FULL_TIME:
+        empHrs = 8;
+        break;
+   default:
+        empHrs = 0;
+}
 EmpWage = empHrs * EMP_RATE_PER_HOUR;
 System.out.println("Emp Wage: " + EmpWage);
 
