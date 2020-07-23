ef0d02fb418860eb3ca9fdb2ae47c5d303fbe645
abhishek <abhishek96890@gmail.com>
2020-07-23 21:40:26 +0530

    ADDED:DailyWage

--- Emp.java
+++ Emp.java
@@ -3,10 +3,16 @@ public class Emp{
 public static void main(String args[])
 {
 int IS_FULL_TIME = 1;
+int EMP_RATE_PER_HOUR = 20;
+int empHrs = 0;
+int empwage = 0;
 double empcheck = Math.floor(Math.random() * 10) % 2;
-if (empcheck == IS_FULL_TIME)
-    System.out.println("Employee is Present");
+if (Empcheck == IS_FULL_TIME)
+     empHrs =  8;
 else
-    System.out.println("Employee is Abscent");
-  }
+     empHrs = 0;
+EmpWage = empHrs * EMP_RATE_PER_HOUR;
+System.out.println("Emp Wage: " + EmpWage);
+
+}
 }
