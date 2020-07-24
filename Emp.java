f13d1b4860166a6a98c4f8e5e91e3c53c5ccaf66
abhishek <abhishek96890@gmail.com>
2020-07-24 22:39:19 +0530

    ADDED=Calculate

--- Emp.java
+++ Emp.java
@@ -3,12 +3,15 @@ public class Emp{
    public static final int IS_FULL_TIME = 2;
    public static final int EMP_RATE_PER_HOUR = 20;
    public static final int NUM_OF_WORKING_DAYS =2;
+   public static final int MAX_HRS_IN_MONTH = 10;
 public static void main(String args[])
 {
 
-int empHrs = 0, empwage = 0, totalEmpWage=0;
-for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
+int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
 { 
+
+totalWorkingDays++;
 int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 switch (empCheck) 
 {
@@ -21,10 +24,10 @@ switch (empCheck)
    default:
         empHrs = 0;
 }
-empwage = empHrs * EMP_RATE_PER_HOUR;
-totalEmpWage += empwage;
-System.out.println("Emp Wage: " + empwage);
+totalEmpHrs += empHrs;
+System.out.println("Day#: " + totalWorkingDays + " Emp Hr: " +empHrs);
 }
+int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
 System.out.println("Total Emp Wage: " + totalEmpWage);
 }
 }
