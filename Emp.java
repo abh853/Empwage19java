85ffcf46a88ff72a4ee21b34dc90b225271c3fd5
abhishek <abhishek96890@gmail.com>
2020-07-24 17:58:41 +0530

    ADDED=dailywage

--- Emp.java
+++ Emp.java
@@ -1,33 +1,8 @@
 public class Emp{
-   public static final int IS_PART_TIME = 1;
-   public static final int IS_FULL_TIME = 2;
-   public static final int EMP_RATE_PER_HOUR = 20;
-   public static final int NUM_OF_WORKING_DAYS =2;
+   
 public static void main(String args[])
 {
-<<<<<<< HEAD
 
-int empHrs = 0, empwage = 0, totalEmpWage=0;
-for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
-{ 
-int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-switch (empcheck) 
-{
-   case IS_PART_TIME:
-        empHrs = 4;
-        break;
-   case IS_FULL_TIME:
-        empHrs = 8;
-        break;
-   default:
-        empHrs = 0;
-}
-EmpWage = empHrs * EMP_RATE_PER_HOUR;
-totalEmpWage += empWage;
-System.out.println("Emp Wage: " + EmpWage);
-}
-System.out.println("Total Emp Wage: " + totalEmpWage);
-=======
 int IS_FULL_TIME = 1;
 int EMP_RATE_PER_HOUR = 20;
 int empHrs = 0;
@@ -40,6 +15,5 @@ else
 empwage = empHrs * EMP_RATE_PER_HOUR;
 System.out.println("Emp Wage: " + empwage);
 
->>>>>>> UC2_DailyWage
 }
 }
