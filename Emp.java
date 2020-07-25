afb2cea10caadd28d5162dfcd1dedcad7f8f985b
abhishek <abhishek96890@gmail.com>
2020-07-25 08:15:19 +0530

    ADDED=Refractor

--- Emp.java
+++ Emp.java
@@ -5,7 +5,7 @@ public class Emp{
      public static final int EMP_RATE_PER_HOUR = 20;
      public static final int NUM_OF_WORKING_DAYS = 2;
      public static final int MAX_HRS_IN_MONTH = 10;
-public static void main(String args[])
+public static int Emp()
 {
 
 int empHrs = 0;
@@ -20,20 +20,24 @@ totalWorkingDays++;
 int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 switch (empCheck) 
 {
-   case IS_PART_TIME:
-        empHrs = 4;
-        break;
-   case IS_FULL_TIME:
-        empHrs = 8;
-        break;
-   default:
-        empHrs = 0;
+case IS_PART_TIME:
+empHrs = 4;
+break;
+case IS_FULL_TIME:
+empHrs = 8;
+break;
+default:
+empHrs = 0;
 }
 totalEmpHrs += empHrs;
 System.out.println("Day#: " + totalWorkingDays + " Emp Hr: " +empHrs);
 }
 int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-System.out.println("Total Emp Wage: " + totalEmpWage);
-
+System.out.println("total Emp Wage: " + totalEmpWage);
+return totalEmpWage;
+}
+public static void main(String args[])
+{
+Emp();
 }
 }
